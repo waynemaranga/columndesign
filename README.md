d301e6eb5775605e1c6b23bd70a7090e45143647
waynemaranga <wmrngcd@gmail.com>
2024-05-20 04:48:22 +0300

    🔁 Rebuild: +4 nb & README

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# COLUMNS
+# STEEL COLUMN DESIGN
 
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![Jupyter](https://img.shields.io/badge/Jupyter-F37626.svg?&style=for-the-badge&logo=Jupyter&logoColor=white)
+<!-- ![Postgres]() ...coming soon -->
+
+- BS 5950-1:2000 and EN 1993-1-1:2005
+
+- Jupyter notebooks using [handcalcs](https://github.com/connorferster/handcalcs) and [forallpeople](https://connorferster.github.io/forallpeople/index.html)
+- `handcalcs` - render Python calculation code automatically in Latex how one might format their calculation by hand: write the symbolic formula, followed by numeric substitutions, and then the result.
+- `forallpeople` - a Python library for representing the SI base units to enable easy-to-use, units-aware calculations. Also incl. US Customary Units
+- Crash course on how to use both on YouTube:
+  - [Introduction to handcalcs: Absolute Beginners Guide](https://youtu.be/ZNFhLCWqA_g?feature=shared)
+  - [Engineering Calculations: Handcalcs-on-Jupyter vs. Excel](https://youtu.be/n9Uzy3Eb-XI?feature=shared)
+  -
+
+## Notebooks `.ipynb`
+
+1. Starter Notebook [0](/0-starter.ipynb)
+2. Struts [1](/struts.ipynb)
+3. Beam-Columns [2](/beam_col.ipynb)
+
+## 📝 in a `.ipynb` means the value should be edited
+
+- probably a manual calc, read from a table, or not coded
+
+## Setup
 
 ```bash
 # Create virtualenv using bash
@@ -8,3 +33,10 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 ```
+
+## Etc
+
+- [Greek symbols](https://github.com/connorferster/handcalcs?tab=readme-ov-file#greek-symbols)
+- [TeX code, without the render](https://github.com/connorferster/handcalcs?tab=readme-ov-file#greek-symbols)
+- [Handcalcs for scripts i.e as a `@decorator`](https://github.com/connorferster/handcalcs?tab=readme-ov-file#basic-usage-2-as-a-decorator-on-your-functions-handcalc)
+- [Pint: make units easy](https://pint.readthedocs.io/en/stable/)
--- modules/strength.py
+++ modules/strength.py
@@ -1,28 +1,28 @@
-def design_strength(steel_grade: int, T: float):
-    """Return design strength of a section given thickness of thickest member. So far only implemented for T"""
+def design_strength(thickness: int, steel_grade: int = 275):
+    """Return design strength of a section given thickness of thickest member. So far only implemented for thickness"""
     design_strength: int = steel_grade
     if steel_grade == 275:
         # fmt: off
-        if T<16: design_strength = 275 
-        elif 16<=T<40: design_strength = 265 
-        elif 40<=T<63: design_strength = 255 
-        elif 63<=T<80: design_strength = 245 
-        elif 80<=T<100: design_strength = 235 
-        elif 100<=T<150: design_strength = 225 
+        if thickness<16: design_strength = 275 
+        elif 16<=thickness<40: design_strength = 265 
+        elif 40<=thickness<63: design_strength = 255 
+        elif 63<=thickness<80: design_strength = 245 
+        elif 80<=thickness<100: design_strength = 235 
+        elif 100<=thickness<150: design_strength = 225 
     elif steel_grade == 355:
         # fmt: off
-        if T<16: design_strength = 355 
-        elif 16<=T<40: design_strength = 345 
-        elif 40<=T<63: design_strength = 335 
-        elif 63<=T<80: design_strength = 325 
-        elif 80<=T<100: design_strength = 315 
-        elif 100<=T<150: design_strength = 295
+        if thickness<16: design_strength = 355 
+        elif 16<=thickness<40: design_strength = 345 
+        elif 40<=thickness<63: design_strength = 335 
+        elif 63<=thickness<80: design_strength = 325 
+        elif 80<=thickness<100: design_strength = 315 
+        elif 100<=thickness<150: design_strength = 295
     elif steel_grade == 460:
         # fmt: off
-        if T<16: design_strength = 460
-        elif 16<=T<40: design_strength = 440 
-        elif 40<=T<63: design_strength = 430 
-        elif 63<=T<80: design_strength = 410 
-        elif 80<=T<100: design_strength = 400
+        if thickness<16: design_strength = 460
+        elif 16<=thickness<40: design_strength = 440 
+        elif 40<=thickness<63: design_strength = 430 
+        elif 63<=thickness<80: design_strength = 410 
+        elif 80<=thickness<100: design_strength = 400
 
     return design_strength
